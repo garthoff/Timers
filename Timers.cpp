db8abed76c574e6543e4fc58ae893199fc8e6927
sprae <saperwl@o2.pl>
2011-11-03 16:36:13 +0100

    Czyszczenie kodu

--- Timers.cpp
+++ Timers.cpp
@@ -9,35 +9,32 @@ void NOP(void)
 TimerPool::TimerPool(byte size)
 {
 	_size = size;
-	funcs = (timerFunc *) malloc(sizeof(timerFunc)*size);
-	intervals = (unsigned long *) malloc(sizeof(unsigned long)*size);
-	begin_times = (unsigned long *) malloc(sizeof(unsigned long)*size);
+	_elements = (TimerElement *) malloc(sizeof(TimerElement)*size);
+
 	for (int i=0; i<_size; i++)
 	{
-		funcs[i] = NOP;
-		intervals[i] = 0;
-		begin_times[i] = 0;
+		_elements[i].func = NOP;
+		_elements[i].interval = 0;
+		_elements[i].begin_time = 0;
 	}
 }
 
 TimerPool::~TimerPool()
 {
-	free(funcs);
-	free(intervals);
-	free(begin_times);
+	free(_elements);
 }
 
 void TimerPool::connect(byte slot, unsigned long interval, timerFunc func)
 {
-	funcs[slot] = func;
-	intervals[slot] = interval;
-	begin_times[slot] = millis();
+	_elements[slot].func = func;
+	_elements[slot].interval = interval;
+	_elements[slot].begin_time = millis();
 }
 
 void TimerPool::setInterval(byte slot, unsigned long interval)
 {
-	intervals[slot] = interval;
-	begin_times[slot] = millis();
+	_elements[slot].interval = interval;
+	_elements[slot].begin_time = millis();
 }
 
 void TimerPool::update(void)
@@ -46,16 +43,17 @@ void TimerPool::update(void)
 	
 	for (int i=0; i<_size; i++)
 	{
-	  long long delta_time = actual_time - begin_times[i];
-	  if (delta_time < 0)
-	  {
-	    delta_time += 0xffffffff;
-	    delta_time += 1;
-	  }
-		if (intervals[i] > 0 && delta_time >= intervals[i])
+		long long delta_time = actual_time - _elements[i].begin_time;
+
+		if (delta_time < 0)
 		{
-			funcs[i]();
-			begin_times[i] = actual_time;
+			delta_time += 0xffffffff;
+			delta_time += 1;
+		}
+		if (_elements[i].interval > 0 && delta_time >= _elements[i].interval)
+		{
+			_elements[i].func();
+			_elements[i].begin_time = actual_time;
 		}
 	}
 }
@@ -65,23 +63,23 @@ LimitedTimerPool::LimitedTimerPool(void)
 {
 	for (int i=0; i<STATIC_SIZE; i++)
 	{
-		funcs[i] = NOP;
-		intervals[i] = 0;
-		begin_times[i] = 0;
+		_elements[i].func = NOP;
+		_elements[i].interval = 0;
+		_elements[i].begin_time = 0;
 	}
 }
 
 void LimitedTimerPool::connect(byte slot, unsigned long interval, timerFunc func)
 {
-	funcs[slot] = func;
-	intervals[slot] = interval;
-	begin_times[slot] = millis();
+	_elements[slot].func = func;
+	_elements[slot].interval = interval;
+	_elements[slot].begin_time = millis();
 }
 
 void LimitedTimerPool::setInterval(byte slot, unsigned long interval)
 {
-	intervals[slot] = interval;
-	begin_times[slot] = millis();
+	_elements[slot].interval = interval;
+	_elements[slot].begin_time = millis();
 }
 
 void LimitedTimerPool::update(void)
@@ -90,16 +88,17 @@ void LimitedTimerPool::update(void)
 	
 	for (int i=0; i<STATIC_SIZE; i++)
 	{
-	  long long delta_time = actual_time - begin_times[i];
+	  long long delta_time = actual_time - _elements[i].begin_time;
+	  
 	  if (delta_time < 0)
 	  {
 	    delta_time += 0xffffffff;
 	    delta_time += 1;
 	  }
-		if (intervals[i] > 0 && delta_time >= intervals[i])
+		if (_elements[i].interval > 0 && delta_time >= _elements[i].interval)
 		{
-			funcs[i]();
-			begin_times[i] = actual_time;
+			_elements[i].func();
+			_elements[i].begin_time = actual_time;
 		}
 	}
 }
